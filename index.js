138b18ad8baf92def3c0031028b4422279043b66
tihitinaw B <Thtnawbuze@gmail.com>
2025-03-06 15:03:34 +0300

    first code js

---
+++ index.js
@@ -0,0 +1,11 @@
+// If Customer has more than 100 points
+// they are 'gold' customer, otherwise,
+// they are 'silver' customer
+
+
+// Ternary Operator
+let points = 110;
+
+let types = points > 100 ? 'gold' : 'silver';
+
+console.log(types);
