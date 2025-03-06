b67a0dad695cdf60e9aff4b400449d4d400d01aa
tihitinaw B <Thtnawbuze@gmail.com>
2025-03-06 15:27:26 +0300

    Logical Oppertion

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 // logical Operator
+// Logical AND (&&) - Returns TRUE if both operands are TRUE
+// Logical OR (||) - Returns TRUE if one of the operands is TRUE
+// Logical NOT (!) - Returns TRUE if the operand is FALSE
 
 let highIncome = true;
 let goodCreditScore = true;
