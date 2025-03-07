1712bcdc54162ec65434cf3b42a69e80231f5d59
abdisa <abdisawake5@gmail.com>
2025-03-07 08:35:29 +0300

    conditional statements

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-let a = 'red';
-let b = 'blue';
+// Conditonal Statements
 
-let c = a ;
+let hour =13;
+
+if (hour >= 6 && hour < 12) {
+    console.log('Good Morning');
+}
+else if (hour >= 12 && hour < 18) {
+    console.log('Good Afternoon');
+}   
+else {
+    console.log('Good Evening');
+}
 
 
-a = b;
-b = c;
-
-console.log(a);
-console.log(b);
