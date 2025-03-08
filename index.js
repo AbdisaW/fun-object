cf70225e01d283c2f7f5a89d277d98572874c8d6
abdisa <abdisawake5@gmail.com>
2025-03-08 12:11:03 +0300

    for in and for of

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// for -in loop
 const person= {
     name: 'John',
     age: 30
@@ -9,3 +9,7 @@ for(let key in person) console.log(key, person[key])
 const colors = ['red', 'green', 'blue'];
 
 for(let i in colors) console.log(i,colors[i]);
+
+// for-of loop
+
+for(let color of colors) console.log(color);
