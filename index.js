32b5a9b79290ca799de7c9714c299c1af0c1433b
abdisa <abdisawake5@gmail.com>
2025-03-09 13:48:12 +0300

    Algorithm of fuzzBuzz

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-let landScape = isLandScape(800, 800);
-console.log(landScape);
+// FizzBuzz algorithm
 
-function isLandScape(width, height) {
-    return width > height;
+// Divisible by 3 => Fizz;
+// Divisible by 5 => BUZZ;
+// Divisible by both 3 and 5 => FizzBuzz
+
+// Not divisible by 3 or 5 => input
+// Not a nmber => 'NaN
+
+const outPut = fizzBuzz(3);
+console.log(outPut);
+function fizzBuzz(input){
+    if(typeof input !== 'number')
+        return NaN
+    if(input % 3 === 0 && input % 5 === 0)
+        return 'fizzBuzz';
+    if(input % 3 === 0 )
+        return 'fizz';
+    
+    if(input % 5 === 0) 
+        return 'Buzz';
+
+    return input;
+
 
 }
