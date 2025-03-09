2a655870958cbae57df5479edc9b3ba0c5abe7aa
abdisa <abdisawake5@gmail.com>
2025-03-09 22:57:27 +0300

    limit number

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
+showNumbers(10);
+function showNumbers(limit){
+    
+    for (let index = 0; index <= limit; index++) {
 
-checkSpeed(180)
-function checkSpeed(speed){
-  const speedLimiter = 70;
-  const kmPerPoints =5;
+        const message = (index % 2 === 0) ? 'EVEN' : "ODD";
+        console.log(index, message)
+      
+       
 
-  if(speed < speedLimiter + kmPerPoints)
-    console.log( 'OK')
-  else{
-    const points = Math.floor((speed- speedLimiter)/kmPerPoints);
-    if( points >= 12)
-        console.log ('Licences Suspended')
-    else{
-        console.log("Points", points)
+        
     }
     
-  }
-
 }
