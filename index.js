802235eaea8f1ea7b321a0029591b1c7b5fb81d1
Abdi <Thtnawbuze@gmail.com>
2025-04-16 04:11:22 +0300

    star with row

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
+showStarts(5)
 
-const marks =[80, 80, 50, 80, 70];
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks){
-    let sum = 0;
-   
-   for(let i =0; i < marks.length; i++){
-    sum += marks[i];
-   }
-   let Average = sum / marks.length ;
-   if(Average >=0 && Average <= 59){
-     return 'C';
+function showStarts(rows){
+  for(let row = 1; row <= rows; row++){
+    let pattern ='';
+    for(let i = 0; i < row; i++){
+      pattern += "*"
+      
     }
-   else if(Average >=60 && Average <= 69){
-    return'D';
-   }
-   else if(Average >=70 && Average <= 79){
-    return'C';
-   }else if(Average >=80 && Average <= 89){
-    return'B';
-   }else if(Average >=90 && Average <= 100){
-    return'A';
-   }
-   else{
-    return 'the averge is not correct ';
-   }
-   
+    console.log(pattern)
+  }
 }
